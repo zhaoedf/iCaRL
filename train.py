f7929b67847d0b0b54412247e6b3abd2b3556277
zhaoedf <610318832@qq.com>
2021-11-21 20:39:06 +0800

    implement iCaRL roughly, code needs to beatify

--- train.py
+++ train.py
@@ -22,18 +22,18 @@ from mlflow.tracking import MlflowClient
 # from continuum.tasks import split_train_val
 # from continuum.datasets import MNIST, CIFAR10
 from data.incremental_scenario import incremental_scenario
+from continuum import TaskSet
 
 from args.args_trainer import args_trainer
 from args.args_model import args_model
-# TODO  mlfow save hyperparams
 
 
-from utils.auxiliary_func import setup_logger
+from utils.auxiliary_func import setup_logger, get_class_mean
 
 
 
 # seed
-seed_everything(args_model.seed, workers=True)
+# seed_everything(args_model.seed, workers=True)
 
 # gpu
 device = "cuda:"+args_trainer.gpus if torch.cuda.is_available() else "cpu"
@@ -76,7 +76,7 @@ inc_scenario = incremental_scenario(
     total_memory_size = args_model.total_memory_size
 )
 train_scenario, test_scenario, memory = inc_scenario.get_incremental_scenarios(logger)
-
+# memory.herding_method = herd_closest_to_barycenter
 
 
 # loss function
@@ -85,154 +85,207 @@ loss_func = F.binary_cross_entropy_with_logits
 # acc & loss metrics
 train_epoch_acc = Accuracy().to(device)
 test_epoch_acc = Accuracy().to(device)
+test_NME_acc = Accuracy().to(device)
 train_epoch_loss = MeanMetric().to(device)
 train_total_loss = MeanMetric().to(device)
 
 
+try:
+    nb_seen_classes = args_model.initial_increment
+    avg_incremental_acc = np.array([])
+    old_model = None
+    for task_id, taskset in enumerate(train_scenario):
+        
+        # update clssifier output dim
+        print(f'update fc dims to {nb_seen_classes}')
+        model.update_fc(nb_seen_classes)
+        model = model.to(device)
+        
+        # add exemplars to train_set
+        if task_id > 0:
+            mem_x, mem_y, mem_t = memory.get()
+            taskset.add_samples(mem_x, mem_y, mem_t)
+        
+        # data
+        train_set = taskset
+        test_set = test_scenario[:task_id+1]
+        
+        train_loader = torch.utils.data.DataLoader(
+            dataset=taskset,
+            batch_size=args_model.batch_size,
+            shuffle=True,
+            num_workers=args_model.num_workers,
+            drop_last=False
+        )
+        test_loader = torch.utils.data.DataLoader(
+            dataset=test_set,
+            batch_size=args_model.batch_size,
+            shuffle=True,
+            num_workers=args_model.num_workers,
+            drop_last=False
+        )
+        nme_test_loader = torch.utils.data.DataLoader(
+            dataset=test_set,
+            batch_size=args_model.batch_size,
+            shuffle=True,
+            num_workers=args_model.num_workers,
+            drop_last=False
+        )
 
-nb_seen_classes = args_model.initial_increment
-avg_incremental_acc = np.array([])
-old_model = None
-for task_id, taskset in enumerate(train_scenario):
-    
-    # update clssifier output dim
-    print(f'update fc dims to {nb_seen_classes}')
-    model.update_fc(nb_seen_classes)
-    model = model.to(device)
-    
-    # add exemplars to train_set
-    if task_id > 0:
+        # optimizer & scheduler
+        optimizer = torch.optim.SGD(
+            model.parameters(), lr=args_model.learning_rate, momentum=0.9, weight_decay=5e-5
+        )
+        lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(
+            optimizer, milestones=[49,63], gamma=0.2
+        )
+        
+        for epoch in range(args_trainer.max_epochs):
+            
+            # train
+            model.train()
+            for idx, batch in enumerate(train_loader):
+                x, y, t = batch
+                x, y = x.to(device), y.to(device) # changhong style
+                logits = model(x)['logits']
+                y_one_hot = F.one_hot(y, num_classes=logits.shape[1]).type_as(logits)  # expand_as 更优雅 TODO
+                
+                if old_model is None:
+                    loss = loss_func(logits, y_one_hot)
+                else:
+                    old_onehots = torch.sigmoid(old_model(x)['logits'].detach())
+                    new_onehots = y_one_hot.clone()
+                    # logger.info(f'{new_onehots.shape} {old_onehots.shape}')
+                    new_onehots[:, :nb_seen_classes-args_model.increment] = old_onehots
+                    loss = loss_func(logits, new_onehots)
+                
+                train_epoch_loss.update(loss)
+                train_total_loss.update(loss)
+                train_epoch_acc.update(torch.sigmoid(logits), y) # y_hat: [bs, nb_classes]   y: [bs,]  [checked]
+                # if task_id == 1: logger.info(f'{y.shape}, {logits.shape}, {y.unique()}')
+
+
+                # if idx % args_trainer.log_every_n_steps == 0:
+                #     logger.info(f'Epoch: {epoch}/{args_trainer.max_epochs} batch: {idx}, loss: {loss.item()}')
+                #     logger.info(f'train_acc: {train_acc}')
+
+                # update parameters
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+            # update lr_scheduler
+            lr_scheduler.step() # should update every epoch instead of step! otherwise it will decay based on step instead of epoch! see source code in doc.
+        
+            logger.info('*'*75)
+            # logger.info('one epoch end')
+            logger.info(f'Task: {task_id}, Epoch: {epoch+1}/{args_trainer.max_epochs}')
+            logger.info(f'train_epoch_loss => {train_epoch_loss.compute().item()} train_total_loss => {train_total_loss.compute().item()}')
+            logger.info(f'train_epoch_acc => {train_epoch_acc.compute()} ')
+            logger.info(f'{lr_scheduler.get_last_lr()}')
+            logger.info('*'*75)
+            mlflow_logger.log_metrics({f'task{task_id}_train_epoch_loss': train_total_loss.compute().item()}, step=epoch)
+            mlflow_logger.log_metrics({f'task{task_id}_train_epoch_acc': train_epoch_acc.compute().item()}, step=epoch)
+            # client.log_metric(run_id, key=f'task{task_id}_train_epoch_loss', value=train_total_loss.compute().item(), step=epoch)
+            # client.log_metric(run_id, key=f'task{task_id}_train_epoch_acc', value=train_Accuracy.compute().item(), step=epoch)
+
+        
+        
+            # logger.info('*'*75)
+            # logger.info(f'one increment end')
+            # test
+            model.eval()
+            for idx, batch in enumerate(test_loader):
+                x, y, t = batch
+                x, y = x.to(device), y.to(device) # changhong style
+                y_hat = torch.sigmoid(model(x)['logits']) 
+                y_one_hot = F.one_hot(y, num_classes=y_hat.shape[1]).type_as(y_hat)
+                loss = F.binary_cross_entropy(y_hat, y_one_hot)  # binary_cross_entropy_with_logits is loss func with sigmoid inside.
+                test_epoch_acc.update(y_hat, y)
+            # model.train()
+            
+            # logger.info(f'Task: {task_id}, Epoch: {epoch+1}/{args_trainer.max_epochs}')
+            # logger.info(f'avg_test_acc for [0:{nb_seen_classes}]  {avg_incremental_acc[-1]}')
+            acc_epoch = test_epoch_acc.compute().item()
+            logger.info(f'test_epoch_acc => {acc_epoch}')
+            mlflow_logger.log_metrics({f'task{task_id}_test_epoch_acc': acc_epoch}, step=epoch)
+            if epoch+1 == args_trainer.max_epochs:
+                logger.info('@'*50)
+                logger.info(f'one increment end')
+                avg_incremental_acc = np.append(avg_incremental_acc, acc_epoch)
+                logger.info(f'avg_incremental_acc for [0:{nb_seen_classes}] => {avg_incremental_acc[-1]}')  # avg_incremental_acc.mean()
+                mlflow_logger.log_metrics({'avg_incremental_acc': avg_incremental_acc[-1]*100}, step=task_id)
+                logger.info('@'*50)
+            
+            # client.log_metric(run_id, key='avg_test_acc', value=test_Accuracy.compute().item(), step=task_id)
+            # client.log_metric(run_id, key='avg_incremental_acc', value=avg_incremental_acc.mean(), step=task_id)
+            train_epoch_loss.reset()
+            train_epoch_acc.reset()
+            test_epoch_acc.reset()
+            logger.info('*'*75)
+        
+        # constructing new exemplar set
+        # model.eval()
+        # get_extract_features = lambda x, model=model: model.extract_vector(x)
+        features = []
+        temp_set = TaskSet(*train_scenario[task_id].get_raw_samples(), test_set.trsf, data_type=test_set.data_type)
+        loader =  torch.utils.data.DataLoader(temp_set, shuffle=False, batch_size=args_model.batch_size)
+        for idx, (x,y,t) in enumerate(loader):
+            # print(y.shape)
+            features.append(model.extract_vector(x.to(device)).cpu().detach().numpy())
+        features = np.concatenate(tuple(features))
+        # logger.info(f'{len(features)}')
+        memory.add(
+            *train_scenario[task_id].get_raw_samples(), features
+        )
+        old_model = model.copy().freeze()
+        # logger.info(f'{memory.memory_per_class}')
+        
+        # NME prediction
+        all_features = []
+        # if task_id > 0:
         mem_x, mem_y, mem_t = memory.get()
-        taskset.add_samples(mem_x, mem_y, mem_t)
-    
-    # data
-    train_set = taskset
-    test_set = test_scenario[:task_id+1]
-    
-    train_loader = torch.utils.data.DataLoader(
-        dataset=taskset,
-        batch_size=args_model.batch_size,
-        shuffle=True,
-        num_workers=args_model.num_workers,
-        drop_last=False
-    )
-    test_loader = torch.utils.data.DataLoader(
-        dataset=test_set,
-        batch_size=args_model.batch_size,
-        shuffle=True,
-        num_workers=args_model.num_workers,
-        drop_last=False
-    )
+        memory_set = TaskSet(mem_x, mem_y, mem_t, test_set.trsf, data_type=test_set.data_type)
+        # print(taskset.data_type) # TaskType.IMAGE_ARRAY
 
-    # optimizer & scheduler
-    optimizer = torch.optim.SGD(
-        model.parameters(), lr=args_model.learning_rate, momentum=0.9, weight_decay=5e-5
-    )
-    lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(
-        optimizer, milestones=[49,63], gamma=0.2
-    )
-    
-    for epoch in range(args_trainer.max_epochs):
+        another_loader = torch.utils.data.DataLoader(memory_set, shuffle=False, batch_size=args_model.batch_size)
+        for idx, (x,y,t) in enumerate(another_loader):
+            # print(x.shape)
+            all_features.append(model.extract_vector(x.to(device)).cpu().detach().numpy())
+        all_features = np.concatenate(tuple(all_features))
+        class_means = np.expand_dims(get_class_mean(mem_y, all_features), axis=0) # .get() returns x,y,z
+        # else:
+        #     class_means = np.expand_dims(get_class_mean(train_scenario[task_id].get_raw_samples()[1], features), axis=0)
         
-        # train
-        model.train()
-        for idx, batch in enumerate(train_loader):
+        correct = 0
+        total = 0
+        for idx, batch in enumerate(nme_test_loader):
             x, y, t = batch
-            x, y = x.to(device), y.to(device) # changhong style
-            logits = model(x)['logits']
-            y_one_hot = F.one_hot(y, num_classes=logits.shape[1]).type_as(logits)  # expand_as 更优雅 TODO
-            
-            if old_model is None:
-                loss = loss_func(logits, y_one_hot)
-            else:
-                old_onehots = torch.sigmoid(old_model(x)['logits'].detach())
-                new_onehots = y_one_hot.clone()
-                # logger.info(f'{new_onehots.shape} {old_onehots.shape}')
-                new_onehots[:, :nb_seen_classes-args_model.increment] = old_onehots
-                loss = loss_func(logits, new_onehots)
-            
-            train_epoch_loss.update(loss)
-            train_total_loss.update(loss)
-            train_epoch_acc.update(torch.sigmoid(logits), y) # y_hat: [bs, nb_classes]   y: [bs,]  [checked]
-            # if task_id == 1: logger.info(f'{y.shape}, {logits.shape}, {y.unique()}')
+            # x, y = x.to(device), y.to(device)
+            feature_vector = np.expand_dims(model.extract_vector(x.to(device)).cpu().detach().numpy(), axis=1) # (bs, feature_dim)
+            dist_to_mean = np.linalg.norm(class_means - feature_vector, axis=2) # (bs, nb_classes)
+            preds = dist_to_mean.argsort()[:, 0]  # default: ascend    
+            # print(dist_to_mean.shape, preds.shape,(class_means - feature_vector).shape)    
+            correct += np.count_nonzero(preds==y.numpy())
+            total += y.shape[0]
+            # print(correct, preds[0], y[0])
+        nme_acc = (correct / total)*100
+        logger.info(f'{nme_acc}, {correct}')
+        mlflow_logger.log_metrics({'nme_acc': nme_acc}, step=task_id)
+            # x, y = x.to(device), y.to(device) # changhong style
+            # y_hat = torch.sigmoid(model(x)['logits']) 
+            # y_one_hot = F.one_hot(y, num_classes=y_hat.shape[1]).type_as(y_hat)
+            # loss = F.binary_cross_entropy(y_hat, y_one_hot)  # binary_cross_entropy_with_logits is loss func with sigmoid inside.
+            # test_epoch_acc.update(y_hat, y)
+
+        nb_seen_classes += args_model.increment
+    
+    mlflow_logger.experiment.log_artifact(run_id, os.path.join(log_path, run_id)+'.log')
+    mlflow_logger.finalize(status='FINISHED')
+except KeyboardInterrupt:
+    print('hello')
+    mlflow_logger.finalize(status='KILLED')
+    # mlflow_logger.experiment.set_terminated(run_id, "KILLED")
+    mlflow_logger.experiment.log_artifact(run_id, os.path.join(log_path, run_id)+'.log')
 
 
-            # if idx % args_trainer.log_every_n_steps == 0:
-            #     logger.info(f'Epoch: {epoch}/{args_trainer.max_epochs} batch: {idx}, loss: {loss.item()}')
-            #     logger.info(f'train_acc: {train_acc}')
-
-            # update parameters
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-        # update lr_scheduler
-        lr_scheduler.step() # should update every epoch instead of step! otherwise it will decay based on step instead of epoch! see source code in doc.
-    
-        logger.info('*'*75)
-        # logger.info('one epoch end')
-        logger.info(f'Task: {task_id}, Epoch: {epoch+1}/{args_trainer.max_epochs}')
-        logger.info(f'train_epoch_loss => {train_epoch_loss.compute().item()} train_total_loss => {train_total_loss.compute().item()}')
-        logger.info(f'train_epoch_acc => {train_epoch_acc.compute()} ')
-        logger.info(f'{lr_scheduler.get_lr()}')
-        logger.info('*'*75)
-        mlflow_logger.log_metrics({f'task{task_id}_train_epoch_loss': train_total_loss.compute().item()}, step=epoch)
-        mlflow_logger.log_metrics({f'task{task_id}_train_epoch_acc': train_epoch_acc.compute().item()}, step=epoch)
-        # client.log_metric(run_id, key=f'task{task_id}_train_epoch_loss', value=train_total_loss.compute().item(), step=epoch)
-        # client.log_metric(run_id, key=f'task{task_id}_train_epoch_acc', value=train_Accuracy.compute().item(), step=epoch)
-
-    
-    
-        # logger.info('*'*75)
-        # logger.info(f'one increment end')
-        # test
-        model.eval()
-        for idx, batch in enumerate(test_loader):
-            x, y, t = batch
-            x, y = x.to(device), y.to(device) # changhong style
-            y_hat = torch.sigmoid(model(x)['logits']) 
-            y_one_hot = F.one_hot(y, num_classes=y_hat.shape[1]).type_as(y_hat)
-            loss = F.binary_cross_entropy(y_hat, y_one_hot)  # binary_cross_entropy_with_logits is loss func with sigmoid inside.
-            test_epoch_acc.update(y_hat, y)
-        # model.train()
-        
-        # logger.info(f'Task: {task_id}, Epoch: {epoch+1}/{args_trainer.max_epochs}')
-        # logger.info(f'avg_test_acc for [0:{nb_seen_classes}]  {avg_incremental_acc[-1]}')
-        acc_epoch = test_epoch_acc.compute().item()
-        logger.info(f'test_epoch_acc => {acc_epoch}')
-        mlflow_logger.log_metrics({f'task{task_id}_test_epoch_acc': acc_epoch}, step=epoch)
-        if epoch+1 == args_trainer.max_epochs:
-            logger.info('@'*50)
-            logger.info(f'one increment end')
-            avg_incremental_acc = np.append(avg_incremental_acc, acc_epoch)
-            logger.info(f'avg_incremental_acc for [0:{nb_seen_classes}] => {avg_incremental_acc[-1]}')  # avg_incremental_acc.mean()
-            mlflow_logger.log_metrics({'avg_incremental_acc': avg_incremental_acc[-1]*100}, step=task_id)
-            logger.info('@'*50)
-        
-        # client.log_metric(run_id, key='avg_test_acc', value=test_Accuracy.compute().item(), step=task_id)
-        # client.log_metric(run_id, key='avg_incremental_acc', value=avg_incremental_acc.mean(), step=task_id)
-        train_epoch_loss.reset()
-        train_epoch_acc.reset()
-        test_epoch_acc.reset()
-        logger.info('*'*75)
-    
-    # constructing new exemplar set
-    # model.eval()
-    # get_extract_features = lambda x, model=model: model.extract_vector(x)
-    features = []
-    loader =  torch.utils.data.DataLoader(taskset, shuffle=False, batch_size=args_model.batch_size)
-    for idx, (x,y,t) in enumerate(loader):
-        print(y.shape)
-        features.append(model.extract_vector(x.to(device)).cpu().detach().numpy())
-    features = np.concatenate(tuple(features))
-    # logger.info(f'{len(features)}')
-    memory.add(
-        *train_scenario[task_id].get_raw_samples(), features
-    )
-    old_model = model.copy().freeze()
-    logger.info(f'{memory.memory_per_class}')
-    
-    nb_seen_classes += args_model.increment
-    # one increment end
-
-mlflow_logger.finalize(status='FINISHED')
 # client.set_terminated(run_id=run_id, status='FINISHED')
--- utils/auxiliary_func.py
+++ utils/auxiliary_func.py
@@ -40,7 +40,7 @@ def setup_logger(log_path, mlflow_runid):
     ch.setLevel(logging.DEBUG) # 指定被处理的信息级别为最低级DEBUG，低于level级别的信息将被忽略
     
     # 输出到file
-    fh = logging.FileHandler(os.path.join(log_path, mlflow_runid), mode='w', encoding='utf-8')  # a指追加模式,w为覆盖模式
+    fh = logging.FileHandler(os.path.join(log_path, mlflow_runid+'.log'), mode='w', encoding='utf-8')  # a指追加模式,w为覆盖模式
     fh.setFormatter(formatter)
     fh.setLevel(logging.DEBUG)
     
@@ -58,7 +58,7 @@ def setup_logger(log_path, mlflow_runid):
 
 def get_class_mean(
     y: np.ndarray,
-    t: np.ndarray,
+    # t: np.ndarray,
     features: np.ndarray,
 ) -> np.ndarray:
     """Herd the samples whose features is the closest to their class mean.
@@ -67,19 +67,28 @@ def get_class_mean(
     :param features: Features of shape (nb_samples, nb_dim).
     :return: The class prototype vector.
     """
+    # print(features.shape, y.shape)
+    assert features.shape[0] == y.shape[0]
     if len(features.shape) != 2:
         raise ValueError(f"Expected features to have 2 dimensions, not {len(features.shape)}d.")
     # indexes = []
 
     means = []
     for class_id in np.unique(y):
+        # print(len(np.unique(y)))
         class_indexes = np.where(y == class_id)[0]
         class_features = features[class_indexes] # from y.shape(above), it is easy to know the shape of "feature" is [nb_samples, feature_dim]
-        class_mean = np.mean(class_features, axis=1) # , keepdims=True
+        # print(class_features.shape, features.shape)
+        class_mean = np.mean(class_features, axis=0) # , keepdims=True # *row vector*
+        # print(class_mean.shape)
+        # print(class_mean.shape)
 
         means.append(class_mean)
-        # dist_to_mean = np.linalg.norm(class_mean - class_features, axis=1)
+        # dist_to_mean = np.linalg.norm(class_mean - class_features, axis=1) # *row vector*
+        # print(dist_to_mean.shape)
         # tmp_indexes = dist_to_mean.argsort()[:nb_per_class]
         
         # indexes.append(class_indexes[tmp_indexes])
     return np.array(means)
+
+
